eecf0cf28eac1349685ed0c3f7f5dbb3d866837b
dhairyarajbabbar <dhairyarajbabbar@gmail.com>
2023-12-30 01:26:45 +0530

    added the awaited function

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ const corsOptions = {
     credentials: true,
     optionsSuccessStatus: 204,
 };
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 // Use cors middleware with options
 app.use(cors(corsOptions));
 app.get("/", (req, res)=>{
