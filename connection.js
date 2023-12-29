92f4ea637d38441a83090330173a8e882da9fa47
dhairyarajbabbar <dhairyarajbabbar@gmail.com>
2023-12-29 05:33:57 +0530

    changed database

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
-// const url="mongodb+srv://dhairyarajbabbar:dJsTd7ByyyOp8UQo@cluster0.pe4gl9r.mongodb.net/?retryWrites=true&w=majority"
-const url="mongodb://127.0.0.1:27017/splitwise_db";
+const url="mongodb+srv://dhairyarajbabbar:dJsTd7ByyyOp8UQo@cluster0.pe4gl9r.mongodb.net/?retryWrites=true&w=majority"
+// const url="mongodb://127.0.0.1:27017/splitwise_db";
 const connectToMongoDb=(async ()=>{
     return mongoose.connect(url).then ( () => {
         console.log("mongodb connected");
